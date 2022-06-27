a1e57621bcf83e3c73160d6ebdf5ff5e2ce75758
AnDrEwHaLiEm <an.roooof@gmail.com>
2022-06-27 15:43:20 +0200

    d

--- src/BusinessLogic/Ticket/Ticket.js
+++ src/BusinessLogic/Ticket/Ticket.js
@@ -8,7 +8,7 @@ class Ticket extends Edit {
     async payACost(req, res) {
         try {
             const { _id, paid } = req.body;
-            const ticket = await this.Model.findByIdAndUpdate(_id, paid, { new: true });
+            const ticket = await this.Model.findByIdAndUpdate(_id, {paid}, { new: true });
             return res.status(200).send(ticket);
         } catch (error) {
             return res.sendStatus(400);
